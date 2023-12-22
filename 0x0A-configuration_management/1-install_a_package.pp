8282dc38eca574403ab145bf7c5703fcb71e2229
Amine Lamuadni <W7equwE5@gmail.com>
2023-12-22 15:49:46 -0800

    Update 1-install_a_package.pp file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,6 +2,6 @@
 
 # Package resource type is used to manage software packages
 package { 'Flask':
-  ensure   => '2.1.0',         # Ensure Flask version 2.1.0 is installed
-  provider => 'pip3',          # Specify pip3 as the package provider
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
