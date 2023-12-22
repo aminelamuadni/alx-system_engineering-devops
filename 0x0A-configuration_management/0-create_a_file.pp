8f94db2a325b6e3c1f02715622a7aba99cdba1eb
Amine Lamuadni <W7equwE5@gmail.com>
2023-12-22 15:46:03 -0800

    Update 0-create_a_file.pp file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,6 @@
+# This manifest creates a file at the specified path with defined properties
+
+# File resource type is used to manage files
 file { '/tmp/school':
   ensure  => file,
   path    => '/tmp/school',
