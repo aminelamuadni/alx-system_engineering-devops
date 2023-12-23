2514f4abdd96f0690d0f3c1bff8d56ddd6bc3bdf
Amine Lamuadni <W7equwE5@gmail.com>
2023-12-22 16:12:47 -0800

    Update 1-install_a_package.pp file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,8 @@
-##!/usr/bin/pup
+#!/usr/bin/pup
 # This manifest ensures that a specific version of Flask is installed
+
 # Package resource type is used to manage software packages
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => pip3,
+  ensure   => '2.1.0',         # Ensure Flask version 2.1.0 is installed
+  provider => 'pip3',          # Specify pip3 as the package provider
 }
