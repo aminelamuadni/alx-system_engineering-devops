0884eaa17c36637787e05febea4242e95bb10be8
Amine Lamuadni <W7equwE5@gmail.com>
2023-12-22 17:35:47 -0800

    Update 2-execute_a_command.pp file

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
 # Kills a process named 'killmenow' using the pkill command
 exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => ['/usr/bin', '/usr/sbin'],
-  return => [0, 1],
+  command   => 'pkill -f killmenow',
+  path      => ['/usr/bin', '/usr/sbin'],
+  returns   => ['0', '1'],
+  logoutput => true,
 }
