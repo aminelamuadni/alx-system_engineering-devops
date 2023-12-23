5db3091934f4da400fe5b881179b1c037d530ab0
Amine Lamuadni <W7equwE5@gmail.com>
2023-12-22 17:10:18 -0800

    Update 2-execute_a_command.pp file

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,7 +2,6 @@
 
 # Exec resource type is used to execute commands
 exec { 'killmenow':
-  command     => 'pkill -f killmenow',    # Command to kill the 'killmenow' process
-  path        => ['/bin', '/usr/bin', '/usr/local/bin'], # Path where the command is executed
-  refreshonly => true,                    # Only execute this command when notified by another resource
+  command     => 'pkill -f killmenow',
+  path        => '/bin', '/usr/bin',
 }
