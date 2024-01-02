7894d859bb6d4da4aff39c24e3c53be497154477
Amine Lamuadni <W7equwE5@gmail.com>
2024-01-01 16:50:29 -0800

    Update 100-puppet_ssh_config.pp file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,16 +2,16 @@
 # - Sets the SSH client to use the private key ~/.ssh/school
 # - Disables password authentication for SSH
 
+include stdlib
+
 file_line { 'Set identity file':
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/school',
-  match  => '^[#]*[\s]*IdentityFile.*$',
+  line   => 'IdentityFile ~/.ssh/school',
   ensure => present,
 }
 
 file_line { 'Disable password authentication':
   path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-  match  => '^[#]*[\s]*PasswordAuthentication.*$',
+  line   => 'PasswordAuthentication no',
   ensure => present,
 }
