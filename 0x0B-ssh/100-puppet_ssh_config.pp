287c65e4d2489586ebf1da84a6a7e46949f81111
Amine Lamuadni <W7equwE5@gmail.com>
2024-01-01 16:47:53 -0800

    Add 100-puppet_ssh_config.pp file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,17 @@
-file_line { 'Declare identity file':
-  path => '/home/vagrant/.ssh/config',
-  line => 'IdentityFile ~/.ssh/school',
+# Puppet manifest to configure SSH client settings
+# - Sets the SSH client to use the private key ~/.ssh/school
+# - Disables password authentication for SSH
+
+file_line { 'Set identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+  match  => '^[#]*[\s]*IdentityFile.*$',
   ensure => present,
 }
 
-file_line { 'Turn off passwd auth':
-  path => '/home/vagrant/.ssh/config',
-  line => 'PasswordAuthentication no',
+file_line { 'Disable password authentication':
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  match  => '^[#]*[\s]*PasswordAuthentication.*$',
   ensure => present,
 }
