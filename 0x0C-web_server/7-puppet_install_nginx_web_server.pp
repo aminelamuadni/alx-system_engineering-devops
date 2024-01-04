68bcc9dc537da94eed9bca0599af61d2d06317de
Amine Lamuadni <W7equwE5@gmail.com>
2024-01-04 04:50:42 -0800

    Update 7-puppet_install_nginx_web_server.pp file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -20,18 +20,10 @@ file { '/var/www/html/index.nginx-debian.html':
     notify  => Service['nginx'],
 }
 
-# Create a custom 404 page
-file { '/var/www/html/custom_404.html':
-    ensure  => file,
-    content => "Ceci n'est pas une page",
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-}
-
 # Configure redirection and custom 404 error page
-exec { 'configure_nginx':
-    command => "sed -i '/server_name _;/a location /redirect_me { return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4; }' /etc/nginx/sites-available/default && sed -i '/server_name _;/a error_page 404 /custom_404.html;' /etc/nginx/sites-available/default",
+exec { 'nginx_301_redirect':
+    command => "sed -i '/server_name _;/a location /redirect_me { return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4; }' /etc/nginx/sites-available/default",
     require => Package['nginx'],
     notify  => Service['nginx'],
-    unless  => "grep -q 'location /redirect_me' /etc/nginx/sites-available/default",
+    unless  => "grep -q 'return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4' /etc/nginx/sites-available/default",
 }
