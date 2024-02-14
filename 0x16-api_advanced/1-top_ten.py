79e11c782e90d7839ef46313cf2922076215fb68
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-14 02:49:32 +0100

    Update 1-top_ten.py file

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module to query the Reddit API for the top ten hot posts of a subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {
+        'User-Agent': ('Python:alx-system_engineering-devops:v1.0 '
+                       '(by /u/AmineLamuadni)')
+    }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            print(None)
+            return
+        posts = response.json().get("data", {}).get("children", [])
+        for post in posts:
+            print(post.get("data", {}).get("title"))
+    except requests.RequestException:
+        print(None)
+
+
+if __name__ == "__main__":
+    pass
