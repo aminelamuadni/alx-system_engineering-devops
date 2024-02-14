a6e5c39d1b638f3f0b05acf3dc67ceb530c0b092
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-14 02:39:34 +0100

    Add 0-subs.py file

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module to query the Reddit API
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers for a given
+    subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        'User-Agent': ('Python:alx-system_engineering-devops:v1.0 '
+                       '(by /u/AmineLamuadni)')
+    }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return 0
+        return response.json().get("data", {}).get("subscribers", 0)
+    except requests.RequestException:
+        return 0
+
+
+if __name__ == "__main__":
+    pass
