cedc34f01b6b9418c4d65c76f906a7d5cb788583
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-14 05:34:51 +0100

    Update 1-top_ten.py file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,14 +9,18 @@ def top_ten(subreddit):
     """
     Prints the titles of the first 10 hot posts listed for a given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {
         'User-Agent': ('Python:alx-system_engineering-devops:v1.0 '
                        '(by /u/AmineLamuadni)')
     }
+    params = {
+        "limit": 10
+    }
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
         if response.status_code != 200:
             print(None)
             return
@@ -28,7 +32,3 @@ def top_ten(subreddit):
             print(post.get("data", {}).get("title"))
     except requests.RequestException:
         print(None)
-
-
-if __name__ == "__main__":
-    pass
