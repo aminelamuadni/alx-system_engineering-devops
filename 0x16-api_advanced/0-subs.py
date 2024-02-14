ea2770892abd26990ce251c9d3943f4348759ce2
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-14 05:47:10 +0100

    Update 0-subs.py file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,6 +10,9 @@ def number_of_subscribers(subreddit):
     Queries the Reddit API and returns the number of subscribers for a given
     subreddit.
     """
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         'User-Agent': ('Python:alx-system_engineering-devops:v1.0 '
