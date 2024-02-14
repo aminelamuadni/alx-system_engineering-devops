720262eb77b5b4148b77cf78d74ce3cba7f44667
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-14 06:34:36 +0100

    Add 0-strace_is_your_friend.pp file

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,14 @@
+# 0-strace_is_your_friend.pp
+# Fixes a typo in the wp-settings.php file for a WordPress installation
+
+class wordpress_typo_fix {
+
+  exec { 'fix-wp-settings-typo':
+    command => "sudo sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+    path    => ['/bin', '/usr/bin', '/usr/local/bin'],
+    onlyif  => "sudo grep -q 'phpp' /var/www/html/wp-settings.php",
+  }
+
+}
+
+include wordpress_typo_fix
