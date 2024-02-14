4f3ee8759be491e0626fb2dd1c30e5867e645b34
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-14 22:50:57 +0100

    Update 0-strace_is_your_friend.pp file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,7 +2,7 @@
 # This Puppet manifest corrects a typo in the WordPress configuration file
 
 exec { 'correct_wp_settings_typo':
-  command => "sudo sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path => '/usr/local/bin/:/bin/',
-  unless => "sudo grep -qv 'phpp' /var/www/html/wp-settings.php"
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => ['/usr/bin', '/usr/sbin'],
+  unless  => "grep -qv 'phpp' /var/www/html/wp-settings.php",
 }
