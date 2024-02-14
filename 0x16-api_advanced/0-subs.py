882dd60c89e75efe169bdbebd693c22e8a4fbd02
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-14 05:24:04 +0100

    Update 0-subs.py file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -20,6 +20,8 @@ def number_of_subscribers(subreddit):
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code != 200:
             return 0
+        if not response.text:
+            return 0
         return response.json().get("data", {}).get("subscribers", 0)
     except requests.RequestException:
         return 0
