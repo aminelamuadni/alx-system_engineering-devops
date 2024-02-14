b2703338db636587e5a5b9c5807d4170aface832
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-14 22:40:40 +0100

    Update 0-strace_is_your_friend.pp file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -5,7 +5,7 @@ class wordpress_typo_fix {
 
   exec { 'fix-wp-settings-typo':
     command => "sudo sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-    path    => ['/usr/bin', '/usr/sbin'],
+    path    => '/usr/local/bin/:/bin/',
     onlyif  => "sudo grep -q 'phpp' /var/www/html/wp-settings.php",
   }
 
