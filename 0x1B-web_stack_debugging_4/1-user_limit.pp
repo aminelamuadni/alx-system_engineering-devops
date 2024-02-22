8bd5a45bddad4048aaf830501de63527afe164bc
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-22 18:48:29 +0100

    Add 1-user_limit.pp file

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,5 @@
+exec { 'change-os-configuration-for-holberton-user':
+  command => "/usr/bin/env sed -i '/^# End of file/i\\holberton soft nofile 4096\\nholberton hard nofile 8192' /etc/security/limits.conf",
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+  unless  => "/bin/grep -q 'holberton .* nofile' /etc/security/limits.conf",
+}
