669c87ac7659aeb2e8736d1565e9d70db2b79cfb
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-22 18:52:37 +0100

    Update 1-user_limit.pp file

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,4 @@
+# Increase file descriptor limits for the holberton user
 exec { 'change-os-configuration-for-holberton-user':
   command => "/usr/bin/env sed -i '/^# End of file/i\\holberton soft nofile 4096\\nholberton hard nofile 8192' /etc/security/limits.conf",
   path    => ['/bin', '/usr/bin', '/usr/sbin'],
