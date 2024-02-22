796183a524272985353197885a47a7dcc1280f33
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-22 18:44:35 +0100

    Update 0-the_sky_is_the_limit_not.pp file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,6 @@
-# Fixes an nginx site that can't handle multiple concurrent requests
-# by increasing the file descriptor limit and restarting nginx
-
-exec { 'increase-ulimit-for-nginx':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
-  path    => ['/usr/bin', '/usr/sbin', '/bin'],
+# Puppet manifest to optimize Nginx for handling high concurrent load
+exec { 'fix--for-nginx':
+  command => "sed -i 's/worker_processes auto;/worker_processes 8;/' /etc/nginx/nginx.conf && systemctl restart nginx",
+  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  unless  => "grep -q 'worker_processes 8;' /etc/nginx/nginx.conf",
 }
