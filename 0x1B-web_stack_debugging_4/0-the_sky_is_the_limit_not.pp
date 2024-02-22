0270ebf26bcbf8b9e8f23825ab504a665279aac9
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-22 18:24:33 +0100

    Update 0-the_sky_is_the_limit_not.pp file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,21 +4,21 @@
 # Update worker_processes
 exec { 'increase-worker_processes':
   command => "sed -i 's/worker_processes  1;/worker_processes  auto;/' /etc/nginx/nginx.conf",
-  path    => ['/bin/', '/usr/bin/'],
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
   unless  => "grep -q 'worker_processes  auto;' /etc/nginx/nginx.conf",
 }
 
 # Update worker_connections
 exec { 'increase-worker_connections':
   command => "sed -i 's/worker_connections  1024;/worker_connections  2048;/' /etc/nginx/nginx.conf",
-  path    => ['/bin/', '/usr/bin/'],
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
   unless  => "grep -q 'worker_connections  2048;' /etc/nginx/nginx.conf",
 }
 
 # Reload Nginx to apply changes
 exec { 'reload-nginx':
   command     => 'nginx -s reload',
-  path        => ['/bin/', '/usr/sbin/', '/usr/bin/'],
+  path        => ['/bin', '/usr/bin', '/usr/sbin'],
   refreshonly => true,
-  subscribe   => [ Exec['increase-worker_processes'], Exec['increase-worker_connections'] ],
+  subscribe   => [Exec['increase-worker_processes'], Exec['increase-worker_connections']],
 }
