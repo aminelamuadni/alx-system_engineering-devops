7067b326b5794803894201cda650136eec2a9b5a
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-23 09:28:50 +0100

    Update 0-the_sky_is_the_limit_not.pp file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,7 +3,8 @@ exec { 'fix--for-nginx':
   command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/',
 }
+
 exec { 'restart-nginx':
-  command     => '/etc/init.d/nginx restart',
-  path        => '/etc/init.d/',
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
 }
