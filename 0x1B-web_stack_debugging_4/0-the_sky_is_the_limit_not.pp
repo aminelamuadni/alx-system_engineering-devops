2d472e3a7c04412d6eb427028c2dafc9c6efca62
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-23 09:25:51 +0100

    Update 0-the_sky_is_the_limit_not.pp file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,9 @@
 # Puppet manifest to increase nofile limit for Nginx and restart the service
 exec { 'fix--for-nginx':
   command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-  path    => ['/bin', '/usr/bin', '/usr/sbin'],
-  unless  => "grep -q '4096' /etc/default/nginx",
+  path    => '/usr/local/bin/:/bin/',
 }
 exec { 'restart-nginx':
   command     => '/etc/init.d/nginx restart',
-  path        => ['/bin', '/usr/bin', '/usr/sbin'],
-  refreshonly => true,
-  subscribe   => Exec['fix--for-nginx'],
+  path        => '/etc/init.d/',
 }
