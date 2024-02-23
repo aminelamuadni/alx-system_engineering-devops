7be6dad7b471fd0a0cfceaf2740d669cd74a5c1a
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-23 08:41:04 +0100

    Update 0-the_sky_is_the_limit_not.pp file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
-# Puppet manifest to optimize Nginx for handling high concurrent load
-exec { 'fix--for-nginx':
-  command => "sed -i 's/worker_processes auto;/worker_processes 8;/' /etc/nginx/nginx.conf && systemctl restart nginx",
-  path    => ['/bin', '/usr/bin', '/usr/sbin'],
-  unless  => "grep -q 'worker_processes 8;' /etc/nginx/nginx.conf",
+# Puppet manifest to comprehensively optimize Nginx for high concurrency handling
+exec { 'adjust-nginx-config-for-concurrency':
+  command => "/bin/bash -c 'sed -i \"/^worker_processes /c\\worker_processes auto;\" /etc/nginx/nginx.conf && sed -i \"/^worker_connections /c\\worker_connections 1024;\" /etc/nginx/nginx.conf && ulimit -n 2048 && systemctl restart nginx'",
+  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  unless  => "grep -q 'worker_processes auto;' /etc/nginx/nginx.conf && grep -q 'worker_connections 1024;' /etc/nginx/nginx.conf",
 }
