dceda6924cd3cb42f41b51995faaf2e75608e195
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-23 09:35:08 +0100

    Update 1-user_limit.pp file

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # Puppet manifest to adjust file descriptor limits for the holberton user
 
 exec { 'set-holberton-soft-nofile':
-  command => 'sed -i '/^holberton soft nofile/s/[[:digit:]]\\+/50000/' /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+  command => "sed -i '/^holberton soft nofile/s/[[:digit:]]\\+/50000/' /etc/security/limits.conf",
+  path    => ['/usr/local/bin/', '/bin/'],
 }
 
 exec { 'set-holberton-hard-nofile':
-  command => 'sed -i '/^holberton hard nofile/s/[[:digit:]]\\+/50000/' /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+  command => "sed -i '/^holberton hard nofile/s/[[:digit:]]\\+/50000/' /etc/security/limits.conf",
+  path    => ['/usr/local/bin/', '/bin/'],
 }
