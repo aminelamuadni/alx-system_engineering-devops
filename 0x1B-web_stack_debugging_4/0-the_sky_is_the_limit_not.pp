e970f2db477855386ffa0a3258d8d25b3df0deef
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-23 08:57:25 +0100

    Update 0-the_sky_is_the_limit_not.pp file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,20 +1,12 @@
 # Puppet manifest for optimizing Nginx to handle high concurrency
-exec { 'optimize-nginx-config':
-  command => "/bin/bash -c 'sed -i \"/^worker_processes /c\\worker_processes auto;\" /etc/nginx/nginx.conf && sed -i \"/^worker_connections /c\\worker_connections 2048;\" /etc/nginx/nginx.conf'",
-  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
-  unless  => "grep -q 'worker_processes auto;' /etc/nginx/nginx.conf && grep -q 'worker_connections 2048;' /etc/nginx/nginx.conf",
-  notify  => Service['nginx'],
-}
 
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => Exec['optimize-nginx-config'],
-}# Puppet manifest for optimizing Nginx to handle high concurrency
 exec { 'optimize-nginx-config':
-  command => "/bin/bash -c 'sed -i \"/^worker_processes /c\\worker_processes auto;\" /etc/nginx/nginx.conf && sed -i \"/^worker_connections /c\\worker_connections 2048;\" /etc/nginx/nginx.conf'",
+  command => "/bin/bash -c 'sed -i \"/^worker_processes /c\\worker_processes auto;\" \
+              /etc/nginx/nginx.conf && sed -i \"/^worker_connections /c\\worker_connections \
+              2048;\" /etc/nginx/nginx.conf'",
   path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
-  unless  => "grep -q 'worker_processes auto;' /etc/nginx/nginx.conf && grep -q 'worker_connections 2048;' /etc/nginx/nginx.conf",
+  unless  => "grep -q 'worker_processes auto;' /etc/nginx/nginx.conf && \
+              grep -q 'worker_connections 2048;' /etc/nginx/nginx.conf",
   notify  => Service['nginx'],
 }
 
